008428460b3db65daa718b2f069030fbe6919601
Alexander Dowling <adowling@nd.edu>
2022-06-03 10:08:32 -0400

    Updated table of contents and build script.

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -1,3 +1,4 @@
 # Run this script from the root directory of this project
 
+python ./scripts/process_notebooks.py
 jb build ../data-and-computing/
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,6 @@
 # Run this script from the root directory of this project
 
+python ./scripts/process_notebooks.py
 jb build ../data-and-computing/
 ghp-import -n -p -f _build/html 
 jb clean ../data-and-computing/
