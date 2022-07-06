52f65df2b2fd8102999ae7e8b3d2e99281254e3b
Alex Dowling <alex@dowlinglab.org>
2022-07-05 22:43:17 -0400

    Updated publish script.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 
 python ./scripts/process_notebooks.py
 jb build ../data-and-computing/
-git add ./_build/html/_images/
-git commit -m "Adding images from build"
+#git add ./_build/html/_images/
+#git commit -m "Adding images from build"
 ghp-import -n -p -f _build/html 
 jb clean ../data-and-computing/
